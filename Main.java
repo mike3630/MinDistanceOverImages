b28e68a6a7ee8b799f3ec9355e1379582737af91
Michael Smith <michaelasmith3@WINDOWS2.SCS.CARLETON.CA>
2017-10-17 09:45:14 -0400

    added greyscale picture output based on pixel distances

--- Main.java
+++ Main.java
@@ -9,18 +9,22 @@ public class Main
 {
   public static void main(String args[]) throws IOException{
     
-    String[] files = {"test1.png", "test2.png", "test3.png", "test4.png",
-      "test5.png", /*"test6.png",*/ "test7.png", "test8.png", "test9.png", "test10.png", "test11.png"};
-    for (int i = 0; i < files.length; i++){
+    String[] inFiles = {"test1.png", "test2.png", "test3.png", "test4.png",
+      "test5.png", "test6.png", "test7.png", "test8.png", "test9.png", "test10.png", "test11.png"};
+    
+    String[] outFiles = {"grey1.png", "grey2.png", "grey3.png", "grey4.png",
+      "grey5.png", "grey6.png", "grey7.png", "grey8.png", "grey9.png", "grey10.png", "grey11.png"};
+    
+    for (int i = 0; i < inFiles.length; i++){
       System.out.println("----------Test " + (i + 1) + " ----------");
-      dijkstraComparison(files[i]);
+      dijkstraComparison(inFiles[i], outFiles[i]);
       System.out.println("----------End Test " + (i + 1) + " ----------");
       System.out.println();
     }
   }
   
-  public static void dijkstraComparison(String fileName) throws IOException{
-    File file= new File(fileName);
+  public static void dijkstraComparison(String inFile, String outFile) throws IOException{
+    File file= new File(inFile);
     BufferedImage image = ImageIO.read(file);
     
     // picture width/height 
@@ -29,6 +33,7 @@ public class Main
     
     // each pixel is a "vertex"
     int[] pixels = new int[width * height];
+    int[] greyScale = new int[width * height];
     
     ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
     
@@ -60,18 +65,41 @@ public class Main
     }
 
     long start = System.currentTimeMillis();
-    double[] distances1 = DijkstraAlgorithm.DijkstraDecreaseKeyUpdate(graph, (width*height)/2, pixels);
+    double[] distances1 = DijkstraAlgorithm.DijkstraDecreaseKeyUpdate(graph, 0, pixels);
     System.out.println("Decrease-Key done. That took " + ((System.currentTimeMillis() - start) / 1000.0) + " seconds.");
     start = System.currentTimeMillis();
-    double[] distances2 = DijkstraAlgorithm.DijkstraOtherUpdate(graph, (width*height)/2, pixels);
+    double[] distances2 = DijkstraAlgorithm.DijkstraOtherUpdate(graph, 0, pixels);
     System.out.println("Re-Enqueue done.   That took " + ((System.currentTimeMillis() - start) / 1000.0) + " seconds.");
     
+    double max = 0;
     for (int i=0; i < distances1.length; i++){
       if (distances1[i] != distances2[i]){
         System.out.println("no work " + i);
         break;
       }
+      if (distances1[i] > max){
+        max = distances1[i];
+      }
     }
+    
+    int greyScaleMod = (int)(max/255) + 1;
+          
+    for (int i = 0; i < distances1.length; i++){
+      int greyScaleVal = 255 - ((int) (distances1[i]/greyScaleMod));
+      greyScale[i] = (255 & 0x000000ff) << 24;
+      greyScale[i] += (greyScaleVal & 0x000000ff) << 16;
+      greyScale[i] += (greyScaleVal & 0x000000ff) << 8;
+      greyScale[i] += (greyScaleVal & 0x000000ff);
+    }
+    
+    for (int i = 0; i < width; i++){
+      for (int j = 0; j < height; j++){
+        image.setRGB(i,j, greyScale[i*height + j]);
+      }
+    }
+    
+    ImageIO.write(image, "png", new File(outFile));
+    
     //System.out.println("dist1: " + Arrays.toString(distances1));
     //System.out.println("dist2: " + Arrays.toString(distances2));
     
